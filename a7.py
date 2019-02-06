d51513e4d89af3b63c76c061ec79b24e6ea3cb98
nyaundid <31975075+nyaundid@users.noreply.github.com>
2019-02-06 16:45:52 -0600

    Add files via upload
    1.Create    **EXACTLY**20 sentences. The maximum length of each sentence is FIVE (5). Two sample sentences may look like: “Excellent work”, “Good students work very hard”.2.Create word embeddings for the vocabulary (i.e. unique words  ) in your sentences. You can choose one OR more word embedding methods from the embedding layer approach, the CBOW approach, the Skip-Gram approach, or the GloVe approach.Create a WORD document to answer the following questions for EACH of the word embedding method you choose: 1.Describe your embedding approach, architecture, and all the    parameters (i.e. epochs, batches) you used. 2.Describe the dimensionality of your word vectors.  3.List the vocabulary in your training set. 4.Use   the   t-SNE method to reduce your word vectors to 2-dimension and p  lot the 2-D points in a figure   . Each point (vector) in the figure MUST be labeled with the original words in your vocabulary. (NOTE: you may need to adjust your training sentences so words with certain meaning cluster closer together in your t-SNE figure)    5.List the training time to train your word embedding model.

---
+++ a7.py
@@ -0,0 +1,48 @@
+
+# coding: utf-8
+
+# In[65]:
+
+
+from textblob import TextBlob
+from gensim.models import Word2Vec
+# define training data
+sentences = [['this' 'is' 'the' 'first' 'sentence'], ['this' 'is' 'the' 'second' 'sentence'], ['the' 'cat' 'in' 'the' 'hat'],
+      ['my' 'dog' 'is' 'mad'], ['i' 'hate' 'animals'], ['i' 'like' 'money'], ['who' 'is' 'the' 'best'],[' when' 'do' 'i' 'leave'],
+      ['my' 'name' 'is' 'jim'], ['i' 'live' 'there'], ['when' 'do' 'we' 'leave'], ['keep' 'it' 'up'], ['the' 'bathroom' 'is' 'clean'],
+      ['dont' 'be' 'rude'], ['have' 'fun'], ['dance' 'like' 'you' 'cant'], ['four' 'trees' 'in' 'the' 'park'], ['i' 'am' 'tall'],
+      ['i' 'am' 'fast'], ['go' 'there' 'now']]
+
+
+# In[67]:
+
+
+import numpy as np
+from sklearn.manifold import TSNE
+from gensim.models import Word2Vec
+from sklearn.decomposition import PCA
+from matplotlib import pyplot
+# define training data
+
+# train model
+model = Word2Vec(sentences, min_count=1)
+# fit a TSNE model to the vectors
+X = model[model.wv.vocab]
+tsne = TSNE(n_components=2)
+X_tsne = tsne.fit_transform(X)
+# create a scatter plot of the projection
+pyplot.scatter(result[:, 0], result[:, 1])
+words = list(model.wv.vocab)
+for i, word in enumerate(words):
+	pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
+pyplot.show()
+import time
+import datetime
+print (datetime.datetime.now().strftime("%y-%m-%d-%H-%M"))
+
+
+# In[68]:
+
+
+X
+
